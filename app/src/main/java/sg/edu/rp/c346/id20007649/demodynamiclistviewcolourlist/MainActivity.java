bdc4e57b33b422a4817ab959c32f7919adc4029f
20007649 <20007649@rp.edu.sg>
2021-06-04 12:31:07 +0800

    Initial commit

---
+++ app/src/main/java/sg/edu/rp/c346/id20007649/demodynamiclistviewcolourlist/MainActivity.java
@@ -0,0 +1,17 @@
+package sg.edu.rp.c346.id20007649.demodynamiclistviewcolourlist;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView tv
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
